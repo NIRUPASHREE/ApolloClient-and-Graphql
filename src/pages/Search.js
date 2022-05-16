db287b19faef6d0f66857bb7dea217b52c7f09a1
NIRUPASHREE <51870577+NIRUPASHREE@users.noreply.github.com>
2022-05-16 13:58:44 +0530

    filter-lazyquery

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -2,12 +2,11 @@ import React, { useState } from 'react';
 import { useLazyQuery, gql } from '@apollo/client';
 
 const GET_CHARACTER_LOCATION = gql`
-  query GetCharacterLocation($name: string!) {
+  query GetCharacterLocation($name: String!) {
     characters(filter: { name: $name }) {
       results {
         location {
           name
-          dimension
         }
       }
     }
@@ -33,6 +32,14 @@ export default function Search() {
       <h1>Search a name for his/her locations</h1>
       <input value={name} onChange={e => setName(e.target.value)} />
       <button onClick={() => getLocation()}>search</button>
+      {loading && <div>....spinnerr....</div>}
+      {error && <div>....error....</div>}
+
+      {data && <ul>
+         {data.characters.results.map((Character) => {
+           return <li>{Character.location.name}</li>
+         })}
+         </ul>}
     </div>
   );
 }
